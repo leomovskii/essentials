9bed5d44d0a2a54c47a9c6606bc98871642876f7
Leomovskii <41242940+leomovskii@users.noreply.github.com>
2025-01-24 14:36:04 +0200

    Update ShowIfDrawer.cs
    Fix issue: bugs if put in array entry

--- Assets/Plugins/Essentials/Attributes/Editor/ShowIfDrawer.cs
+++ Assets/Plugins/Essentials/Attributes/Editor/ShowIfDrawer.cs
@@ -1,70 +1,86 @@
+#if UNITY_EDITOR
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
-using System.Reflection;
 
-#if UNITY_EDITOR
 namespace Essentials {
 
 	[CustomPropertyDrawer(typeof(ShowIfAttribute))]
 	public class ShowIfDrawer : PropertyDrawer {
-
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
 			var showIf = (ShowIfAttribute) attribute;
-			bool shouldShow = false;
-
-			var target = property.serializedObject.targetObject;
-			var memberInfo = target.GetType().GetMember(showIf.Condition, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)[0];
-
-			if (memberInfo != null) {
-				switch (memberInfo.MemberType) {
-					case MemberTypes.Method:
-					var method = (MethodInfo) memberInfo;
-					shouldShow = (bool) method.Invoke(target, null);
-					break;
-
-					case MemberTypes.Field:
-					var field = (FieldInfo) memberInfo;
-					shouldShow = (bool) field.GetValue(target);
-					break;
-
-					case MemberTypes.Property:
-					var propertyInfo = (PropertyInfo) memberInfo;
-					shouldShow = (bool) propertyInfo.GetValue(target);
-					break;
-				}
-			}
-
-			if (shouldShow == showIf.ExpectedValue)
+			bool shouldShow = ShouldShow(property, showIf);
+			if (shouldShow)
 				EditorGUI.PropertyField(position, property, label, true);
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
 			var showIf = (ShowIfAttribute) attribute;
-			var target = property.serializedObject.targetObject;
+			bool shouldShow = ShouldShow(property, showIf);
+			return shouldShow ? EditorGUI.GetPropertyHeight(property, label, true) : 0;
+		}
 
-			bool shouldShow = false;
-			var memberInfo = target.GetType().GetMember(showIf.Condition, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)[0];
+		private bool ShouldShow(SerializedProperty property, ShowIfAttribute showIf) {
+			var target = GetParentObject(property);
+			if (target == null)
+				return false;
 
-			if (memberInfo != null) {
+			var members = target.GetType().GetMember(showIf.Condition, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+			if (members.Any()) {
+				var memberInfo = members[0];
 				switch (memberInfo.MemberType) {
 					case MemberTypes.Method:
 					var method = (MethodInfo) memberInfo;
-					shouldShow = (bool) method.Invoke(target, null);
-					break;
+					return (bool) method.Invoke(target, null);
 
 					case MemberTypes.Field:
 					var field = (FieldInfo) memberInfo;
-					shouldShow = (bool) field.GetValue(target);
-					break;
+					return (bool) field.GetValue(target);
 
 					case MemberTypes.Property:
 					var propertyInfo = (PropertyInfo) memberInfo;
-					shouldShow = (bool) propertyInfo.GetValue(target);
-					break;
+					return (bool) propertyInfo.GetValue(target);
 				}
 			}
+			return false;
+		}
 
-			return shouldShow == showIf.ExpectedValue ? EditorGUI.GetPropertyHeight(property, label, true) : -EditorGUIUtility.standardVerticalSpacing;
+		private object GetParentObject(SerializedProperty property) {
+			var path = property.propertyPath.Replace(".Array.data[", "[");
+			object obj = property.serializedObject.targetObject;
+			var elements = path.Split('.');
+
+			foreach (var element in elements.Take(elements.Length - 1))
+				if (element.Contains("[")) {
+					var elementName = element.Substring(0, element.IndexOf("["));
+					var index = int.Parse(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+					obj = GetValue(obj, elementName, index);
+				} else
+					obj = GetValue(obj, element);
+			return obj;
+		}
+
+		private object GetValue(object source, string name, int index = -1) {
+			if (source == null)
+				return null;
+
+			var type = source.GetType();
+			var field = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+			var property = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+			var value = field != null ? field.GetValue(source) : property?.GetValue(source);
+
+			if (index >= 0 && value is System.Collections.IEnumerable enumerable) {
+				var enumerator = enumerable.GetEnumerator();
+				for (int i = 0; i <= index; i++)
+					if (!enumerator.MoveNext())
+						return null;
+				return enumerator.Current;
+			}
+
+			return value;
 		}
 	}
 }
